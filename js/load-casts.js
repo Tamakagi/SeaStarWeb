1143295f2937bfef367780f28f8c19c1c6d4d15f
tamakagi <t.rmagi0422@gmail.com>
2026-07-03 17:58:10 +0900

    モーダル追加

--- js/load-casts.js
+++ js/load-casts.js
@@ -24,10 +24,39 @@ fetch("data/casts-data.json")
           <p class="cast-card__description">${cast.description}</p>
         </div>
       `;
-
+      //キャストカードにクリックイベントを追加
+      card.addEventListener("click", () => {
+        showModal(cast);
+      });
       castGrid.appendChild(card);
     });
   })
   .catch((error) => {
     console.error("casts-data.json の読み込みに失敗:", error);
   });
+
+// モーダル要素を取得
+const modal = document.getElementById("cast-modal");
+const modalImage = document.getElementById("modal-image");
+const closeModal = document.querySelector(".close");
+
+// モーダルを閉じる関数
+function closeModalHandler() {
+  modal.classList.remove("is-open");
+}
+
+// モーダルを表示する関数
+function showModal(cast) {
+  modalImage.src = `./assets/cast-detail/${cast.image}`;
+  modal.classList.add("is-open");
+}
+
+// クロスボタンでモーダルを閉じる
+closeModal.addEventListener("click", closeModalHandler);
+
+// モーダル外をクリックしたときにも閉じる
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    closeModalHandler();
+  }
+});
